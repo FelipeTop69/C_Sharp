8940124c7ebf466abc2d1bfecdb05524435b8d9d
FelipeAT <pipeaviles1608@gmail.com>
2025-02-04 11:12:31 -0500

    Carpeta vscode de .Net_Angular fue removida del rastero

--- .Net_Angular/WebAPI/WebAPI/obj/Debug/net9.0/WebAPI.AssemblyInfo.cs
+++ .Net_Angular/WebAPI/WebAPI/obj/Debug/net9.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8fbf3dc646b1a856da351b2eb867960aab5ffd9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3e8dd6ea8fcc0b0fe93ab3c45622362526f8ed4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
